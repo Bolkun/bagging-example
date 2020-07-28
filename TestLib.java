1c88da977ec93182de83af4f9df62d5a8429c4a9
bolkun <bolkun@BDV092.slub-dresden.de>
2020-07-28 11:24:14 +0200

    calls of thread safe hashes

--- TestLib.java
+++ TestLib.java
@@ -32,6 +32,7 @@ import org.slf4j.LoggerFactory;
 import com.github.jscancella.domain.Bag;
 import com.github.jscancella.domain.BagBuilder;
 import com.github.jscancella.hash.internal.FileCountAndTotalSizeVistor;
+import com.github.jscancella.hash.BagitChecksumNameMapping;
 
 public class TestLib {
 	
@@ -70,10 +71,21 @@ public class TestLib {
 		Path bagNewDir = Paths.get(slubbagit_path_export);	// NOTE: the data directory MUST be included as this is part of the relative path
 		
 		Path payloadFiles = Paths.get(ie_path_import);
+		
+		//create new thread safe hasher
+		ThreadSafeMD5Hasher newMd5Hasher = new ThreadSafeMD5Hasher();
+		BagitChecksumNameMapping.add(newMd5Hasher.getBagitAlgorithmName(), newMd5Hasher);
+		
+		ThreadSafeSHA512Hasher newSHA512Hasher = new ThreadSafeSHA512Hasher();
+		BagitChecksumNameMapping.add(newSHA512Hasher.getBagitAlgorithmName(), newSHA512Hasher);
+		
+		//ThreadSafeSHA3256Hasher newSha3256Hasher = new ThreadSafeSHA3256Hasher();	
+		//BagitChecksumNameMapping.add(newSha3256Hasher.getBagitAlgorithmName(), newSha3256Hasher);
 	
 		BagBuilder builder = new BagBuilder();
 		builder.addAlgorithm("md5")
 			.addAlgorithm("sha512")
+			//.addAlgorithm("sha3256")	// ERROR com.github.jscancella.domain.BagBuilder - [sha3256] is not supported so it will be ignored. Please add an implementation to BagitChecksumNameMapping.java if you wish to use [sha3256]
 			.addMetadata("foo", "bar")			
 			.addPayloadFile(payloadFiles)
 			.addTagFile(Paths.get(absolute_dir + "/import/meta"))
