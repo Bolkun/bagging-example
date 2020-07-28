a9b407d4b084d8a640c63dd4335894a44869c62f
bolkun <bolkun@BDV092.slub-dresden.de>
2020-07-28 11:35:46 +0200

    refactoring structure

--- ThreadSafeMD5Hasher.java
+++ ThreadSafeMD5Hasher.java
@@ -11,66 +11,65 @@ import java.util.Formatter;
 import com.github.jscancella.hash.Hasher;
 
 public class ThreadSafeMD5Hasher implements Hasher {
-  private static final int _64_KB = 1024 * 64;
-  private static final int CHUNK_SIZE = _64_KB;
-  private MessageDigest messageDigestInstance;
+	private static final int _64_KB = 1024 * 64;
+	private static final int CHUNK_SIZE = _64_KB;
+	private MessageDigest messageDigestInstance;
 
-  @Override
-  public String hash(final Path path) throws IOException{
-    try {
-      final MessageDigest threadLocalMessageDigest = MessageDigest.getInstance("MD5");
-      updateMessageDigest(path, threadLocalMessageDigest);
-      return formatMessageDigest(threadLocalMessageDigest);
-    } catch (NoSuchAlgorithmException e) {
-      throw new IOException(e); //rethrow the error as IOException
-    }
-  }
+	@Override
+	public String hash(final Path path) throws IOException {
+		try {
+			final MessageDigest threadLocalMessageDigest = MessageDigest.getInstance("MD5");
+			updateMessageDigest(path, threadLocalMessageDigest);
+			return formatMessageDigest(threadLocalMessageDigest);
+		} catch (NoSuchAlgorithmException e) {
+			throw new IOException(e); //rethrow the error as IOException
+		}
+	}
   
-  private static void updateMessageDigest(final Path path, final MessageDigest messageDigest) throws IOException{
-    try(InputStream inputStream = new BufferedInputStream(Files.newInputStream(path, StandardOpenOption.READ))){
-      final byte[] buffer = new byte[CHUNK_SIZE];
-      int read = inputStream.read(buffer);
+	private static void updateMessageDigest(final Path path, final MessageDigest messageDigest) throws IOException {
+		try(InputStream inputStream = new BufferedInputStream(Files.newInputStream(path, StandardOpenOption.READ))){
+			final byte[] buffer = new byte[CHUNK_SIZE];
+			int read = inputStream.read(buffer);
 
-      while(read != -1){
-        messageDigest.update(buffer, 0, read);
-        read = inputStream.read(buffer);
-      }
-    }
-  }
+			while(read != -1){
+				messageDigest.update(buffer, 0, read);
+				read = inputStream.read(buffer);
+			}
+		}
+	}
   
-  private static String formatMessageDigest(final MessageDigest messageDigest){
-    try(Formatter formatter = new Formatter()){
-      for (final byte b : messageDigest.digest()) {
-        formatter.format("%02x", b);
-      }
+	private static String formatMessageDigest(final MessageDigest messageDigest) {
+		try(Formatter formatter = new Formatter()){
+			for (final byte b : messageDigest.digest()) {
+				formatter.format("%02x", b);
+			}
       
-      return formatter.toString();
-    }
-  }
+			return formatter.toString();
+		}
+	}
   
-  @Override
-  public String getHash(){
-    return formatMessageDigest(messageDigestInstance);
-  }
+	@Override
+	public String getHash() {
+		return formatMessageDigest(messageDigestInstance);
+	}
 
-  @Override
-  public void update(final byte[] bytes){
-    messageDigestInstance.update(bytes);
-  }
+	@Override
+	public void update(final byte[] bytes) {
+		messageDigestInstance.update(bytes);
+	}
 
-  @Override
-  public void reset(){
-    messageDigestInstance.reset();
-  }
+	@Override
+	public void reset(){
+		messageDigestInstance.reset();
+	}
 
-  @Override
-  public String getBagitAlgorithmName(){
-    return "md5";
-  }
+	@Override
+	public String getBagitAlgorithmName() {
+		return "md5";
+	}
   
-  @Override
-  public void initialize() throws NoSuchAlgorithmException{
-    messageDigestInstance = MessageDigest.getInstance("MD5");
-  }
-
+	@Override
+	public void initialize() throws NoSuchAlgorithmException {
+		messageDigestInstance = MessageDigest.getInstance("MD5");
+	}
 }
